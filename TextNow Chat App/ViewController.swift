fd27fe5fa4689e574e4325eb69920aac139cfc8b
Aaron Treinish <aarontreinish@fastmail.com>
2019-02-13 18:37:00 -0500

    Initial Commit

---
+++ TextNow Chat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextNow Chat App
+//
+//  Created by Aaron Treinish on 2/13/19.
+//  Copyright © 2019 Aaron Treinish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
